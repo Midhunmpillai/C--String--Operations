d8d4222e79984f7d5228e7eede16e286305da1a8
Midhunmpillai <midhunmpillai847@gmail.com>
2025-10-04 16:19:33 +0530

    Recursive String Operation Utility.c

---
+++ Pattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if(num > 0)
+    {
+        for(int i = 0; i < num; i++)
+        {
+            for(int j = 0; j < num; j++)
+            {
+                if(i == 0 || i == num - 1 || j == 0 || j == num - 1)
+                {
+                    printf("* ");
+                }
+                else
+                    printf("  ");
+
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
